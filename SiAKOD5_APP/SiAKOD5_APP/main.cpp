d95d432c8a58c1be67fd1e62a4f04b94a9f4c5c9
SkripnichenkoILYA <skripnichenkoilya@mail.ru>
2019-12-17 20:16:31 +0300

    Create Stack class and Queue class. Change List class

--- SiAKOD5_APP/SiAKOD5_APP/main.cpp
+++ SiAKOD5_APP/SiAKOD5_APP/main.cpp
@@ -1,131 +1,89 @@
 #include <iostream>
 #include <string>
-using namespace std;
-
 
 //////////////////////Односвязный линейный список
 template<typename T>
 class SLLList
 {
 private:
-    template<typename T>
-    class Node
-    {
-    public:
-        Node* next;
-        T data;
-        Node(T data = T(), Node* next = nullptr)
-        {
-            this->data = data;
-            this->next = next;
-        }
-    };
-    int size;
-    Node<T>* head;
-    
+	template<typename T>
+	class Node
+	{
+	public:
+		Node* next;
+		T data;
+		Node(T data = T(), Node* next = nullptr)
+		{
+			this->data = data;
+			this->next = next;
+		}
+	};
+	int size;
+	Node<T>* head;
+
 public:
-    SLLList();
-    ~SLLList();
-    T pop_front();
-    T pop_back();
-    void push_front(T data) { head = new Node<T>(data, head); size++; }
-    void push_back(T data);
-    void insert(T data, int index);
-    void remove_at(int index);
-    void clear() { while (size) pop_front(); }
-    int get_size() { return size; }
-    T& operator[](const int index);
-    void toNext() { head = head->next; }
+	SLLList();
+	~SLLList();
+	T pop_front();
+	T pop_back();
+	void push_front(T data);
+	void push_back(T data);
+	int get_size() { return size; }
 };
 
 template<typename T>
 SLLList<T>::SLLList()
 {
-    size = 0;
-    head = nullptr;
+	size = 0;
+	head = nullptr;
 }
 
 template<typename T>
 SLLList<T>::~SLLList()
 {
-    clear();
+	clear();
 }
 
 template<typename T>
 T SLLList<T>::pop_front()
 {
-    Node<T>* temp = head;
-    head = head->next;
-    T data = temp->data;
-    delete temp;
-    size--;
-    return data;
-}
-
-template<typename T>
-void SLLList<T>::push_back(T data)
-{
-    if (head == nullptr) head = new Node<T>(data);
-    else
-    {
-        Node<T>* current = this->head;
-        while (current->next != nullptr) current = current->next;
-        current->next = new Node<T>(data);
-    }
-    size++;
-}
-
-template<typename T>
-void SLLList<T>::insert(T data, int index)
-{
-    if (index == 0) push_front(data);
-    else
-    {
-        Node<T>* previous = this->head;
-        for (int i = 0; i < index; i++) previous = previous->next;
-        Node<T>* newNode = new Node<T>(data, previous->next);
-        previous->next = newNode;
-        size++;
-    }
-}
-
-template<typename T>
-void SLLList<T>::remove_at(int index)
-{
-    if (index == 0) pop_front();
-    else
-    {
-        Node<T>* previous = this->head;
-        for (int i = 0; i < index; i++) previous = previous->next;
-        Node<T>* toDelete = previous->next;
-        previous->next = toDelete->next;
-        delete toDelete;
-        size--;
-    }
+	Node<T>* temp = head;
+	head = head->next;
+	T data = temp->data;
+	delete temp;
+	size--;
+	return data;
 }
 
 template<typename T>
 T SLLList<T>::pop_back()
 {
-    Node<T>* current = this->head;
-    while (current->next) current = current->next;
-    T data = current->data;
-    current = NULL;
-    size--;
-    return data;
+	Node<T>* current = this->head;
+	while (current->next) current = current->next;
+	T data = current->data;
+	current = NULL;
+	size--;
+	return data;
 }
 
 template<typename T>
-T& SLLList<T>::operator[](const int index)
+void SLLList<T>::push_front(T data)
 {
-    unsigned counter = 0;
-    Node<T>* current = this->head;
-    while (current != nullptr)
-    {
-        if (counter == index) return current->data;
-        current = current->next;
-        counter++;
-    }
+	head = new Node<T>(data, head);
+	size++;
+}
+
+template<typename T>
+void SLLList<T>::push_back(T data)
+{
+	if (head == nullptr) head = new Node<T>(data);
+	else
+	{
+		Node<T>* current = this->head;
+		while (current->next != nullptr) current = current->next;
+		current->next = new Node<T>(data);
+	}
+	size++;
 }
 
 
@@ -133,38 +91,62 @@ T& SLLList<T>::operator[](const int index)
 class Task
 {
 private:
-    string name;
-    int time;
-    int priority;
-    
+	std::string name;
+	unsigned time;
+	unsigned priority;
+
 public:
-    Task()
-    {
-        name = "";
-        time = 0;
-        priority = 1000;
-    }
-    Task(string name, int time, int priority)
-    {
-        this->name = name;
-        this->time = time;
-        this->priority = priority;
-    }
-    string get_name() { return name; }
-    int get_time() { return time; }
-    int get_priority() { return priority; }
-    void countdown() { --time; }
-    bool isFinished() { return time == 0; }
-    void output_status()
-    {
-        cout << "Имя задачи: " << name << " ; время выполнения: " << time << ".\n";
-    }
+	Task()
+	{
+		name = "";
+		time = 0;
+		priority = 1000;
+	}
+	Task(std::string name, int time, int priority)
+	{
+		this->name = name;
+		this->time = time;
+		this->priority = priority;
+	}
+	std::string get_name() { return name; }
+	unsigned get_time() { return time; }
+	int get_priority() { return priority; }
+	void countdown() { --time; }
+	bool isFinished() { return time == 0; }
+	void output_status()
+	{
+		std::cout << "Имя задачи: " << name << " ; время выполнения: " << time << ".\n";
+	}
 };
 
 
 //////////////////////Стек
+template<typename T>
+class Stack
+{
+private:
+	SLLList<T> block;
+
+public:
+	void push(T data) { block.push_front(data); }
+	T pop() { return block.pop_back(); }
+	bool isEmpty() { return block.get_size() == 0; }
+};
+
 
 //////////////////////Очередь
+template<typename T>
+class Queue
+{
+private:
+	SLLList<T> block;
+
+public:
+	void push(T data) { block.push_back(data); }
+	T pop() { return block.pop_front(); }
+	bool isEmpty() { return block.get_size() == 0; }
+};
+
 
 //////////////////////Процессор
 
@@ -172,7 +154,7 @@ public:
 
 int main()
 {
-    setlocale(0, "");
-    
-    return 0;
+	setlocale(0, "");
+
+	return 0;
 }
